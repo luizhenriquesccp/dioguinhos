7704b122abe1caad789423529db564905c2a744a
luizhenriquesccp <147549836+luizhenriquesccp@users.noreply.github.com>
2026-07-12 18:09:58 -0300

    Recusar não é uma opção...

--- assets/js/file.js
+++ assets/js/file.js
@@ -1,3 +1,12 @@
 function randomNumber(min, max) {
-    return (Math.random() * (max - min + 1)) + min + "%";
+    return (Math.random() * (max - min + 1) + min) + "%";
+}
+
+function numVaiNao() {
+    var x = randomNumber(0, 90);
+    var y = randomNumber(0, 90);
+    var button = document.querySelector(".button.secondary");
+    button.style.position = "absolute";
+    button.style.left = x;
+    button.style.top = y;
 }
